fa0bf537c031876dc7b9a5144eb436d20fe3b324
eganovianingsih <eganovia07@mail.unpas.ac.id>
2019-10-10 00:36:14 +0700

    menambahkan folder publi dalam folder phpmvc

--- public/js/script.js
+++ public/js/script.js
@@ -4,8 +4,6 @@ $(function(){
 		$('.modal-footer button[type=submit]').html('Tambah Data');
 	});
 
-
-
     $('.tampilModalUbah').on('click', function(){
 		$('#formModalLabel').html('Ubah Data Mahasiswa');
 		$('.modal-footer button[type=submit]').html('Ubah Data');
