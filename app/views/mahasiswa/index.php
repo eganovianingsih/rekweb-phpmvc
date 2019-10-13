dc70362aba6a6f1f6dca2b9bf05eed658f8c5d78
eganovianingsih <eganovia07@mail.unpas.ac.id>
2019-10-13 20:03:15 +0700

    menambahkan row baru tapi untuk menampung inputanya

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,45 +1,58 @@
 <div class="container mt_3">
 
 
-<div class="row">
-	<div class="col-lg-6">
-	<?php Flasher::flash(); ?>
-	</div>
+		<div class="row">
+			<div class="col-lg-6">
+			<?php Flasher::flash(); ?>
+			</div>
+		</div>
 
-</div>
+
+		<div class="row mb-3">
+		 	<div class="col-lg-6">
+		 		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+						Tambah Data Mahasiswa
+				</button>
+		 	</div>
+		</div>
+
+		<div class="row mb-3">
+		 	<div class="col-lg-6">
+		 		<form action="<?= BASEURL; ?>/mahasiswa/cari" method="post">
+			 		<div class="input-group ">
+					  <input type="text" class="form-control" placeholder="Cari Mahasiswa.." name="keyword" id="keyword" autocomplete="off">
+					  <div class="input-group-append">
+					    <button class="btn btn-primary" type="submit" id="tombolCari">Cari</button>
+					  </div>
+					</div>
+</form>
+		 	</div>
+		</div>
+
+
 	
-<div class="row">
-	<div class="col-lg-6">
-		<!-- Button trigger modal -->
-		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
-	Tambah Data Mahasiswa
-	</button>
-	<br><br>
-		<h3>Daftar Mahasiswa</h3>
-		<ul class="list-group">
-			<?php  foreach( $data['mhs'] as $mhs) : ?>
+ 		<div class="row">
+ 			<div class="col-lg-6">
+				<h3>Daftar Mahasiswa</h3>
+				<ul class="list-group">
+				<?php  foreach( $data['mhs'] as $mhs) : ?>
 			
-	  <li class="list-group-item ">
-	  	<?= $mhs['nama']; ?>
-			  <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>"
-	  			class="badge badge-danger float-right ml-1"
-				  onclick="return confirm('yakin');">Hapus</a>
-				  
-				<a href="<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id']; ?>"
-	  			class="badge badge-success float-right ml-1 tampilModalUbah" data-toggle="modal" data-target="#formModal"data-id="<?= $mhs['id'];?>">Ubah</a>
-
-			 	 <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>"
-	  			class="badge badge-primary float-right ml-1">Detail</a>
-	  		
-	  		
-	  	</li> 
-	  <?php endforeach; ?> 
-	</ul>
-			
-	</div>
-</div>
-
-</div>
+			  <li class="list-group-item ">
+			  	<?= $mhs['nama']; ?>
+					  	<a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>"
+			  			class="badge badge-danger float-right ml-1"
+						onclick="return confirm('yakin');">Hapus</a>  
+						<a href="<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id']; ?>"
+			  			class="badge badge-success float-right ml-1 tampilModalUbah" data-toggle="modal" data-target="#formModal"data-id="<?= $mhs['id'];?>">Ubah</a>
+					 	<a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>"
+			  			class="badge badge-primary float-right ml-1">Detail</a>
+	  				
+			  </li> 
+			 		 <?php endforeach; ?> 
+				</ul>
+			</div>
+		</div>
+	</div>		
 
 	<!-- Modal -->
 <div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="formModal" aria-hidden="true">
