3673040ad12638fccca63f6bf25ab37b302c9571
eganovianingsih <eganovia07@mail.unpas.ac.id>
2019-10-13 20:07:50 +0700

    membuat method cariDataMahasiswa

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -42,32 +42,35 @@
 		$query = "DELETE FROM mahasiswa WHERE id = :id";
 		$this->db->query($query);
 		$this->db->bind('id',$id);
-
 		$this->db->execute();
 
 		return $this->db->rowCount();
 	}
 
+
 	public function ubahDataMahasiswa($data)
 	{
-		$query = "UPDATE mahasiswa SET
-					nama = :nama,
-					nrp = :nrp,
-					email = :email,
-					jurusan = :jurusan
-					WHERE id = :id";
-		$this->db->query($query);
-		$this->db->bind('nama',$data['nama']);
-		$this->db->bind('nrp',$data['nrp']);
-		$this->db->bind('email',$data['email']);
-		$this->db->bind('jurusan',$data['jurusan']);
-		$this->db->bind('id',$data['id']);
+		$query = "UPDATE mahasiswa SET 
+				nama = :nama,
+				nrp = :nrp,
+				email = :email,
+				jurusan = :jurusan
+				WHERE id = :id";
 
-		$this->db->execute();
 
-		return $this->db->rowCount();			
+			$this->db->query($query);
+			$this->db->bind('nama', $data['nama']);
+			$this->db->bind('nrp', $data['nrp']);
+			$this->db->bind('email', $data['email']);
+			$this->db->bind('jurusan', $data['jurusan']);
+			$this->db->bind('id',$data['id']);
+
+			$this->db->execute();
+
+			return $this->db->rowCount();
 	}
 
+			
 	public function cariDataMahasiswa()
 	{
 		$keyword = $_POST['keyword'];
@@ -76,6 +79,7 @@
 		$this->db->bind('keyword', "%$keyword%");
 		return $this->db->resultSet();
 	}
+	
 	}
 
  ?>
